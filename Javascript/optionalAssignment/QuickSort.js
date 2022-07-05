96b1e6328c28c5e2ee96d2a3e734435795bc6960
Tsukuyomi-808 <froilanxx3@gmail.com>
2022-07-04 21:48:13 -1000

    ninja commit

---
+++ Javascript/optionalAssignment/QuickSort.js
@@ -0,0 +1,28 @@
+
+// Hoare's Partitioning 
+
+const QuickSort = (arr) => {
+  if( arr.length < 2 );
+  return arr;
+}
+
+// Recursive
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(QuickSort([6,7,5,8,10,22,34,56,75,34,65,4,14,16]))
