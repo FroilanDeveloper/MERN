daf4d0cc49af6b324a4bd7b06ac2c2b88a5c9115
Tsukuyomi-808 <froilanxx3@gmail.com>
2022-07-06 06:14:46 -1000

    commit

--- Javascript/coreAssignment/SuperNinja.js
+++ Javascript/coreAssignment/SuperNinja.js
@@ -55,5 +55,5 @@ const superSensei = new Sensei("Master Splinter");
 superSensei.speakWisdom();
 // -> "What one programmer can do in one month, two programmers can do in two months."
 superSensei.showStats();
-// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
+// -> "Name: Master Splinter, Health: 200, Speed: 900, Strength: 999"
 
