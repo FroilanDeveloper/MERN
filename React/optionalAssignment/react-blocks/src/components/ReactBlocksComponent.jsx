ecbd17be3f94a426e7f2fa39254224709a9b308b
Tsukuyomi-808 <froilanxx3@gmail.com>
2022-07-07 13:32:51 -1000

    commit

---
+++ React/optionalAssignment/react-blocks/src/components/ReactBlocksComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ReactBlocksComponent extends Component {
+  render() {
+    return (
+      <div>ReactBlocksComponent</div>
+    )
+  }
+}
+
+export default ReactBlocksComponent
