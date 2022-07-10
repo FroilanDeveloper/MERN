bc40e7d99d3111cacc47f9666ed8211f3bc8b8ba
Tsukuyomi-808 <froilanxx3@gmail.com>
2022-07-10 07:07:39 -1000

    commit moreforms

---
+++ React/coreAssignment/moreforms/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MoreForms from './conponents/MoreForms';
+
+function App() {
+  return (
+    <div className="App">
+      <MoreForms/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/coreAssignment/moreforms/src/conponents/MoreForms.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+const MoreForms = (props) => {
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+
+  return(
+    <div classname='container'>
+      <form>
+        <div>
+          <h3>FORM</h3>
+        </div>
+        <h3 classname='answers'>
+          <label>First Name: </label>
+          <input type="text" onChange={(e) => setFirstName(e.target.value)} value={firstname}  />
+          {
+            (firstname.length < 3)?
+            <span style={{color: "blue"}}><h5>First Name should be at least 3 characters!</h5></span>:""
+          }
+        </h3>
+        <h3 classname='answers'>
+          <label>Last Name: </label>
+          <input type="text" onChange={(e) => setLastName(e.target.value)} value={lastname}  />
+          {
+            (lastname.length < 3)?
+            <span style={{color: "purple"}}><h5>Last Name should be at least 3 characters!</h5></span>:""
+          }
+        </h3>
+        <h3 classname='answers'>
+          <label>Email: </label>
+          <input type="text" onChange={(e) => setEmail(e.target.value)} value={email}  />
+          {
+            (email.length < 10)?
+            <span style={{color: "orange"}}><h5>Email should be at least 2 characters!</h5></span>:""
+          }
+        </h3>
+        <h3 classname='answers'>
+          <label>Password: </label>
+          <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+          {
+            (password.length < 15)?
+            <span style={{color: "red"}}><h5>Password should be at least 15 characters!</h5></span>:""
+          }
+        </h3>
+        <h3 classname='answers'>
+          <label>Confirmed Password: </label>
+          <input type="password" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmpassword}  />
+          {
+            (confirmpassword.length === 0)? "":
+            (confirmpassword.length != password)?
+            <span style={{color: "skyblue"}}><h5>This must match password!</h5></span>:""
+          }
+        </h3>
+      </form>
+      <div>
+        <h1>Display</h1>
+        <h3>First Name: {firstname}</h3>
+        <h3>Last Name: {lastname}</h3>
+        <h3>Email: {email}</h3>
+        <h3>Password: {password}</h3>
+        <h3>Confirm Password: {confirmpassword}</h3>
+      </div>
+    </div>
+  )
+}
+
+export default MoreForms
