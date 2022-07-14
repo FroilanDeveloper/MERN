81feb7c63f6699ce50d241e732e10d7a989269b1
Tsukuyomi-808 <froilanxx3@gmail.com>
2022-07-14 08:18:14 -1000

    commit

---
+++ React/coreAssignment/lukeapi/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './views/Header'
+import People from './components/People'
+import Planets from './components/Planets'
+import {Routes, Route} from 'react-router-dom'
+import axios from 'axios'
+
+function App() {
+  return (
+    <div>
+      <h1>Star Wars API</h1>
+      {/* <Routes>
+        
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
